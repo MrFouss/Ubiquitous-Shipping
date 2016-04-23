23074695baea7c162e3c7004a87cd8b78ffd70a7
MrFouss <maxime.brodat@fouss.fr>
2016-04-23 22:49:23 +0200

    Added solver.py

---
+++ ag41_transshipment/solver.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+# File: solver.py
+#
+# By Maxime Brodat <maxime.brodat@fouss.fr>
+#
+# Created: 20/04/2016 by Fouss
+
+"""Solver file for the transshipment solver project"""
+
+from ag41_transshipment.parser import *
+import networkx as nx
