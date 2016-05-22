f9d2126d2275d936b4c40ea3fb8f5023b9752872
th0re <stephane.perrez@gmail.com>
2016-05-22 21:28:24 +0200

    Gap graph function

--- ag41_transshipment/solver.py
+++ ag41_transshipment/solver.py
@@ -9,6 +9,8 @@
 
 """Solver file for the transshipment solver project"""
 
+import networkx as nx
+
 
 def solve(graph):
     """Main solving function"""
@@ -48,3 +50,45 @@ def get_depot_list(graph):
             depot_list.append(i)
 
     return depot_list
+
+
+def get_platform_list(graph):
+    """"Lists all platform nodes"""
+
+    platform_list = []
+    for i in graph.nodes():
+        if graph.node[i]['demand'] == 0:
+            platform_list.append(i)
+
+    return platform_list
+
+
+def get_client_list(graph):
+    """"Lists all platform nodes"""
+
+    client_list = []
+    for i in graph.nodes():
+        if graph.node[i]['demand'] > 0:
+            client_list.append(i)
+
+    return client_list
+
+
+def get_gap_graph(graph):
+    """Details platforms with new nodes and edges"""
+
+    gap_graph = nx.DiGraph()
+    gap_graph.add_nodes_from(graph.nodes)
+    for u, v in graph.edges:
+        if graph.edge[u][v]['flow'] < graph.edge[u][v]['capacity']:
+            gap_graph.add_edge(u, v, id=graph.edge[u][v]['id'],
+                               capacity=graph.edge[u][v]['capacity']-graph.edge[u][v]['flow'],
+                               fixed_cost=graph.edge[u][v]['fixed_cost'], unit_cost=graph.edge[u][v]['unit_cost'],
+                               time=graph.edge[u][v]['time'])
+
+        if graph.edge[u][v]['flow'] > 0:
+            gap_graph.add_edge(v, u, id=graph.edge[u][v]['id'], capacity=graph.edge[u][v]['flow'],
+                               fixed_cost=graph.edge[u][v]['fixed_cost'], unit_cost=graph.edge[u][v]['unit_cost'],
+                               time=graph.edge[u][v]['time'])
+
+    return gap_graph
